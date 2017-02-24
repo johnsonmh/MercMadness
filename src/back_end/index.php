8a44a01e43b91489451dd8de693e4bb12f23955a
Megan Landau <MeganLandau@megans-mbp-2.gateway.pace.com>
2017-02-24 01:16:39 -0500

    fixed side navigation expansion bug

--- src/back_end/index.php
+++ src/back_end/index.php
@@ -1,5 +1,5 @@
 <?php
 echo "This page might be a 'loading' page, intro page, or maybe just go straight to map";
 ?>
-<br><a href="merc.html">Map</a></br>
+<br><a href="../front_end/dashboard.html">Map</a></br>
 <br><a href="back_end/hostTest.php">Data parse</a></br>
