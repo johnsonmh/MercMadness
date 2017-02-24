59c5982fc0b8a2ba0eee221efa59432da6304499
Megan Landau <MeganLandau@megans-mbp-2.gateway.pace.com>
2017-02-24 00:22:45 -0500

    fixed pixel limitation

---
+++ src/config.js
@@ -0,0 +1,3 @@
+var config = {
+    GOOGLE_MAPS_API_KEY : 'AIzaSyCnUHFKV2CT-Aj6w-CQkhfOQjp1Ulcnx0M'
+}
--- src/index.php
+++ src/index.php
@@ -2,3 +2,4 @@
 echo "This page might be a 'loading' page, intro page, or maybe just go straight to map";
 ?>
 <br><a href="merc.html">Map</a></br>
+<br><a href="back_end/hostTest.php">Data parse</a></br>
