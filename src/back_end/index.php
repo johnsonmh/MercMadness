b749d723b6533e781f760d5d765757e91639bc35
Megan Landau <MeganLandau@megans-mbp-2.gateway.pace.com>
2017-02-24 01:26:02 -0500

    edited path to status.dat

--- src/back_end/index.php
+++ src/back_end/index.php
@@ -2,4 +2,4 @@
 echo "This page might be a 'loading' page, intro page, or maybe just go straight to map";
 ?>
 <br><a href="../front_end/dashboard.html">Map</a></br>
-<br><a href="back_end/hostTest.php">Data parse</a></br>
+<br><a href="hostTest.php">Data parse</a></br>
