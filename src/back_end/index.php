6707c22c6ce65653b86fa1e70f8528fec5a6655b
Megan Landau <MeganLandau@megans-mbp-2.gateway.pace.com>
2017-02-24 19:07:27 -0500

    editing hostTest.php

--- src/back_end/index.php
+++ src/back_end/index.php
@@ -1,5 +1,5 @@
 <?php
 echo "This page might be a 'loading' page, intro page, or maybe just go straight to map";
 ?>
-<br><a href="../front_end/dashboard.html">Map</a></br>
+<br><a href="../front_end/dashboard.php">Map</a></br>
 <br><a href="hostTest.php">Data parse</a></br>
