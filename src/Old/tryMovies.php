db0a8a2595479a701f88256aae22de96639b5550
Megan Landau <landaumd@g.cofc.edu>
2017-04-29 00:32:45 -0400

    cleaned up files

--- src/Old/tryMovies.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'movies.php';
-
-$movies = new SimpleXMLElement($xmlstr);
-
-echo $movies->movie[0]->plot;
-?>
--- src/Old/tryingHostStatus.php
+++
@@ -1,1 +0,0 @@
-<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?hostgroup=all&amp;style=hostdetail&amp;hoststatustypes=12" target="<?php echo $link_target;?>">Hosts</a>
